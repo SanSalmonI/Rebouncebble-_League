87e8d2f7739bf559a8d60944600eeb3732980297
LukasVGP <hoplite.arts@gmail.com>
2025-01-26 14:19:29 -0800

    controller support added

--- Assets/Scripts/PlayerControllerP2.cs
+++ Assets/Scripts/PlayerControllerP2.cs
@@ -2,23 +2,6 @@ using UnityEngine;
 
 public class PlayerControllerP2 : PlayerController
 {
-    protected override void GetInput()
-    {
-        // Player 2 specific movement with arrow keys
-        if (Input.GetKey(KeyCode.UpArrow)) inputDirection.z = 1;
-        else if (Input.GetKey(KeyCode.DownArrow)) inputDirection.z = -1;
-        else inputDirection.z = 0;
-
-        // Rotation with left/right arrows
-        if (Input.GetKey(KeyCode.RightArrow)) rotationInput = 1;
-        else if (Input.GetKey(KeyCode.LeftArrow)) rotationInput = -1;
-        else rotationInput = 0;
-
-        // Player 2 specific jump and sprint
-        isJumping = Input.GetKey(KeyCode.RightAlt);
-        isSprinting = Input.GetKey(KeyCode.LeftShift);
-    }
-
     protected override void HandleRotation()
     {
         float rotation = rotationInput * rotationSpeed * Time.deltaTime * 100f;
