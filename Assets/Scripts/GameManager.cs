e60d162a112f0c995ebbb3712f1b0ee8b5a6f8df
Ghuztavo <ghuztavo2002@gmail.com>
2025-01-26 14:32:57 -0800

    bubble default material and game manager
    default material for the bubble fixed and game manager script added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField] public int player1Lives = 3;
+    [SerializeField] public int player2Lives = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
