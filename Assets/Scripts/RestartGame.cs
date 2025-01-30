4d08ba29cc2e108aaf5fbc0f5b71eb3305ff3de6
Salmon <ssalmonize@gmail.com>
2025-01-30 14:24:10 -0800

    first build

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -1,8 +1,5 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
 using UnityEngine.SceneManagement;
-using UnityEditor.Build.Content;
 
 
 public class RestartGame : MonoBehaviour
